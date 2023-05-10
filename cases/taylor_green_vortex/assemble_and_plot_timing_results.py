f07fe5965cf9c6d9099be784c0fca8b36bf2dd25
jbrillon <43619715+jbrillon@users.noreply.github.com>
2023-05-10 15:21:58 -0400

    improving cpu script for 2 sets of poly degree

--- cases/taylor_green_vortex/assemble_and_plot_timing_results.py
+++ cases/taylor_green_vortex/assemble_and_plot_timing_results.py
@@ -13,53 +13,59 @@ sys.path.append(CURRENT_PATH+"../../submodules/quickplotlib/lib"); import quickp
 #=====================================================
 # Helper functions
 #=====================================================
-# p_min=1
-# p_max=15
-# poly_degree=np.arange(p_min,p_max+1)
-# poly_degree=np.array([1,2,3,4,5,6,7,9,10,11])
-poly_degree=np.array([1,2,3,5,6,7,9,10,11]) # for nicer plot
+nCPUs=512
+p_min=1
+p_max=14 #15
+NSFR_poly_degree=np.arange(p_min,p_max+1)
+std_sDG_poly_degree=np.array([1,2,3,4,5,6,7,9,10,11])
 number_of_elements_per_direction=4
 base_directories=["/Users/Julien/julien_phd/cluster-scripts/outputs/jcp/cpu_time_advantage/"]# UPDATE THIS
 
 
 n_sets_of_runs_for_averaging=len(base_directories)
-n_poly_degree=len(poly_degree)
-NSFR_store_cpu_time_per_step = np.zeros((n_sets_of_runs_for_averaging,n_poly_degree))
-std_sDG_store_cpu_time_per_step = np.zeros((n_sets_of_runs_for_averaging,n_poly_degree))
+NSFR_n_poly_degree=len(NSFR_poly_degree)
+std_sDG_n_poly_degree=len(std_sDG_poly_degree)
+NSFR_store_cpu_time_per_step = np.zeros((n_sets_of_runs_for_averaging,NSFR_n_poly_degree))
+std_sDG_store_cpu_time_per_step = np.zeros((n_sets_of_runs_for_averaging,std_sDG_n_poly_degree))
 
 for i,base_dir in enumerate(base_directories):
-    for j,p in enumerate(poly_degree):
+    for j,p in enumerate(std_sDG_poly_degree):
         dofs = number_of_elements_per_direction*(p+1)
         oi = p+1 # overintegration
-        std_sDG_job_name = "viscous_TGV_ILES_std_strong_DG_Roe_GL_OI-%i_dofs0%i_p%i_CFL-0.1_procs512" % (oi,dofs,p)
-        NSFR_job_name = "viscous_TGV_ILES_NSFR_cDG_IR_2PF_GL_OI-%i_dofs0%i_p%i_CFL-0.1_procs512" % (0,dofs,p)
+        std_sDG_job_name = "viscous_TGV_ILES_std_strong_DG_Roe_GL_OI-%i_dofs0%i_p%i_CFL-0.1_procs%i" % (oi,dofs,p,nCPUs)
         std_sDG_cpu_time_per_step = np.loadtxt(base_dir+std_sDG_job_name+"/timer_values.txt",usecols=(2),skiprows=1)
-        NSFR_cpu_time_per_step = np.loadtxt(base_dir+NSFR_job_name+"/timer_values.txt",usecols=(2),skiprows=1)
-        print(NSFR_cpu_time_per_step)
         std_sDG_store_cpu_time_per_step[i,j] = std_sDG_cpu_time_per_step
+
+for i,base_dir in enumerate(base_directories):
+    for j,p in enumerate(NSFR_poly_degree):
+        dofs = number_of_elements_per_direction*(p+1)
+        NSFR_job_name = "viscous_TGV_ILES_NSFR_cDG_IR_2PF_GL_OI-%i_dofs0%i_p%i_CFL-0.1_procs%i" % (0,dofs,p,nCPUs)
+        NSFR_cpu_time_per_step = np.loadtxt(base_dir+NSFR_job_name+"/timer_values.txt",usecols=(2),skiprows=1)
         NSFR_store_cpu_time_per_step[i,j] = NSFR_cpu_time_per_step
 
 # average the values for all sets of runs
-avg_NSFR_store_cpu_time_per_step = np.zeros(n_poly_degree)
-avg_std_sDG_store_cpu_time_per_step = np.zeros(n_poly_degree)
+avg_NSFR_store_cpu_time_per_step = np.zeros(NSFR_n_poly_degree)
+avg_std_sDG_store_cpu_time_per_step = np.zeros(std_sDG_n_poly_degree)
 
-for j,p in enumerate(poly_degree):
+for j,p in enumerate(std_sDG_poly_degree):
     avg_std_sDG_store_cpu_time_per_step[j] = np.average(std_sDG_store_cpu_time_per_step[:,j])
+
+for j,p in enumerate(NSFR_poly_degree):
     avg_NSFR_store_cpu_time_per_step[j] = np.average(NSFR_store_cpu_time_per_step[:,j])
 
 x_store = []
 y_store = []
 labels_store = []
 
-x_store.append(poly_degree)
+x_store.append(std_sDG_poly_degree)
 y_store.append(avg_std_sDG_store_cpu_time_per_step)
 labels_store.append("Strong DG-Roe-GL-OI")
 
-x_store.append(poly_degree)
+x_store.append(NSFR_poly_degree)
 y_store.append(avg_NSFR_store_cpu_time_per_step)
 labels_store.append("$c_{DG}$ NSFR.IR-GL")
 
-title_label="TGV at Re$_{\\infty}=1600$ with $%i^3$ Elements" % 4
+title_label="TGV at Re$_{\\infty}=1600$ with $%i^3$ Elements on %i CPUs" % (4,nCPUs)
 qp.plotfxn(xdata=x_store,ydata=y_store,xlabel="Polynomial Degree",ylabel="CPU Time for One Time Step [s]",
             title_label=title_label,
             fig_directory="figures",figure_filename="cpu_timing",log_axes="y",figure_filetype="pdf",
