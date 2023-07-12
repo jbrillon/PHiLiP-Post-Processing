2845b3bb4f3d7ae5cee3f868adddca3567e5312f
jbrillon <43619715+jbrillon@users.noreply.github.com>
2023-07-12 18:01:19 -0400

    plotting over-integration stability

--- cases/taylor_green_vortex/parse_data_and_generate_plots.py
+++ cases/taylor_green_vortex/parse_data_and_generate_plots.py
@@ -43,6 +43,74 @@ def reinit_inputs():
 #-----------------------------------------------------
 
 #=====================================================
+# DOFs: 64^3 | Over-integration stabilization
+#-----------------------------------------------------
+if(True):
+    # clr_input = ['tab:red','tab:blue','tab:green','tab:orange','tab:purple','tab:brown','tab:pink','tab:gray','tab:olive','tab:cyan']
+    reinit_inputs()
+    data_directory_base="/Users/Julien/NarvalFiles/2023_JCP/"
+    date_for_runs="."
+    figure_subdirectory="2023_JCP"
+    figure_title = "TGV at Re$_{\\infty}=1600$, P$7$, $64^{3}$ DOFs, CFL$=0.10$" # comment to turn off
+    figure_filename_postfix = "64_p7_overintegration_stability"
+    legend_inside_input=True
+    #-----------------------------------------------------
+    subdirectories.append("high_poly_degree_GL_flux_nodes/viscous_TGV_ILES_NSFR_cDG_IR_2PF_GL_OI-0_dofs064_p7_procs512")
+    filenames.append("turbulent_quantities.txt")
+    labels.append("$c_{DG}$ NSFR.IR-GL")
+    black_line_flag.append(False)
+    dashed_line_flag.append(False)
+    #-----------------------------------------------------
+    subdirectories.append("high_poly_degree_GL_flux_nodes/viscous_TGV_ILES_std_strong_DG_Roe_GL_OI-0_dofs064_p7_procs512")
+    filenames.append("turbulent_quantities.txt")
+    labels.append("Strong DG-Roe-GL")
+    black_line_flag.append(False)
+    dashed_line_flag.append(False)
+    #-----------------------------------------------------
+    subdirectories.append("high_poly_degree_GL_flux_nodes/viscous_TGV_ILES_std_strong_DG_Roe_GL_OI-8_dofs064_p7_procs512")
+    filenames.append("turbulent_quantities.txt")
+    labels.append("Strong DG-Roe-GL-OI")
+    black_line_flag.append(False)
+    dashed_line_flag.append(False)
+    #-----------------------------------------------------
+    subdirectories.append("high_poly_degree_GL_flux_nodes/viscous_TGV_ILES_std_strong_DG_Roe_GL_OI-4_dofs064_p7_CFL-0.10_procs512")
+    filenames.append("turbulent_quantities.txt")
+    labels.append("Strong DG-Roe-GL-OI.4")
+    black_line_flag.append(False)
+    dashed_line_flag.append(False)
+    #-----------------------------------------------------
+    subdirectories.append("high_poly_degree_GL_flux_nodes/viscous_TGV_ILES_std_strong_DG_Roe_GL_OI-2_dofs064_p7_CFL-0.10_procs512")
+    filenames.append("turbulent_quantities.txt")
+    labels.append("Strong DG-Roe-GL-OI.2")
+    black_line_flag.append(False)
+    dashed_line_flag.append(False)
+    #-----------------------------------------------------
+    plot_periodic_turbulence(
+        figure_subdirectory,
+        subdirectories,
+        filenames,
+        labels,
+        black_line_flag,
+        dashed_line_flag,
+        figure_directory_base,
+        data_directory_base,
+        plot_reference_result,
+        figure_filename_postfix,
+        figure_title,
+        log_axes_input,
+        legend_on_input,
+        legend_inside_input,
+        nlegendcols_input,
+        # clr_input=clr_input,
+        transparent_legend_input=True,
+        tmax=14.0,
+        legend_fontSize_input=14,
+        solid_and_dashed_lines=False,
+        plot_numerical_dissipation=False)
+    #-----------------------------------------------------
+exit()
+
+#=====================================================
 # DOFs: 96^3 | Over-Integration Accuracy sDG
 #-----------------------------------------------------
 if(True):
