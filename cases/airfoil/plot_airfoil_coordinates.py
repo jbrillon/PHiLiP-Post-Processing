ae56556c0a80cf6a001c6d653f27f7a6771b4fb0
jbrillon <43619715+jbrillon@users.noreply.github.com>
2023-07-13 13:36:44 -0400

    writing the airfoil coordinates

--- cases/airfoil/plot_airfoil_coordinates.py
+++ cases/airfoil/plot_airfoil_coordinates.py
@@ -1,6 +1,3 @@
-import numpy as np
-
-
 #-----------------------------------------------------
 # Import public libraries
 import numpy as np # NumPy: contains basic numerical routines
@@ -12,17 +9,31 @@ import os;CURRENT_PATH = os.path.split(os.path.realpath(__file__))[0]+"/";
 import sys
 sys.path.append(CURRENT_PATH+"../../submodules/quickplotlib/lib"); import quickplotlib as qp
 
+def write_coordinates_only(number_of_points,filename,x,y):
+    file = open(filename,"w")
+    for i in range(0,number_of_points):
+        wstr = "%1.6e %1.6e\n" % (x[i],y[i]) # 6 decimals to be consistent with input csv files
+        file.write(wstr)
+    file.close()
+    return
+
 x_store = []
 y_store = []
 
 chord=10.0
-x,y = np.loadtxt("sd7003-il_alpha_0_deg.csv",skiprows=9,max_rows=61,unpack=True,delimiter=",")
+number_of_points=61
+# 0 degree AoA
+x,y = np.loadtxt("sd7003-il_alpha_0_deg.csv",skiprows=9,max_rows=number_of_points,unpack=True,delimiter=",")
 x_store.append(x/chord)
 y_store.append(y/chord)
-
-x,y = np.loadtxt("sd7003-il_alpha_8_deg.csv",skiprows=9,max_rows=61,unpack=True,delimiter=",")
+# write coordinates normalized by chord length
+write_coordinates_only(number_of_points,"sd7003-il_alpha_0_deg_surface_only_normalized.txt",x/chord,y/chord)
+# 8 degree AoA
+x,y = np.loadtxt("sd7003-il_alpha_8_deg.csv",skiprows=9,max_rows=number_of_points,unpack=True,delimiter=",")
 x_store.append(x/chord)
 y_store.append(y/chord)
+# write coordinates normalized by chord length
+write_coordinates_only(number_of_points,"sd7003-il_alpha_8_deg_surface_only_normalized.txt",x/chord,y/chord)
 
 qp.plotfxn(x_store,y_store,
     figure_filename="sd7003coordinates",
