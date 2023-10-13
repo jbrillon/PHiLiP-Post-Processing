6bfec9282f2ad89068620688186b57ab5528083c
jbrillon <43619715+jbrillon@users.noreply.github.com>
2023-10-13 08:48:01 -0400

    modifying for new shear-improved results with element-wise mean

--- cases/taylor_green_vortex/parse_data_and_generate_plots.py
+++ cases/taylor_green_vortex/parse_data_and_generate_plots.py
@@ -1413,21 +1413,21 @@ if(True):
     subdirectories_for_plot=[\
     "flux_nodes/viscous_TGV_ILES_NSFR_cDG_IR_2PF_GL_OI-0_dofs096_p5_procs512",\
     "sgs_model_GL_flux_nodes/viscous_TGV_LES_SMAG_MC-0.10_NSFR_cDG_IR_2PF_GL_OI-0_dofs096_p5_procs512",\
-    "sgs_model_GL_flux_nodes/viscous_TGV_LES_SI.SMAG_MC-0.10_NSFR_cDG_IR_2PF_GL_OI-0_dofs096_p5_CFL-0.1_procs512",\
     # "sgs_model_GL_flux_nodes/viscous_TGV_LES_filtered_pL2_SMAG_MC-0.10_NSFR_cDG_IR_2PF_GL_OI-0_dofs096_p5_CFL-0.1_procs512",\
     "sgs_model_GL_flux_nodes/viscous_TGV_LES_filtered_pL3_SMAG_MC-0.10_NSFR_cDG_IR_2PF_GL_OI-0_dofs096_p5_CFL-0.1_procs512",\
     # "sgs_model_GL_flux_nodes/viscous_TGV_LES_filtered_pL4_SMAG_MC-0.10_NSFR_cDG_IR_2PF_GL_OI-0_dofs096_p5_CFL-0.1_procs512",\
-    "sgs_model_GL_flux_nodes/viscous_TGV_LES_filtered_pL3_SS.VMS_MC-0.10_NSFR_cDG_IR_2PF_GL_OI-0_dofs096_p5_CFL-0.1_procs512",\
+    "sgs_model_GL_flux_nodes/viscous_TGV_LES_SI.SMAG_MC-0.10_NSFR_cDG_IR_2PF_GL_OI-0_dofs096_p5_CFL-0.1_procs512",\
+    # "sgs_model_GL_flux_nodes/viscous_TGV_LES_filtered_pL3_SS.VMS_MC-0.10_NSFR_cDG_IR_2PF_GL_OI-0_dofs096_p5_CFL-0.1_procs512",\
     ]
     # labels
     labels_for_plot=[\
     "$c_{DG}$ NSFR.IR-GL", \
     "$c_{DG}$ NSFR.IR-GL-Smag. $C_{S}=0.10$", \
-    "$c_{DG}$ NSFR.IR-GL-SI.Smag. $C_{S}=0.10$", \
     # "$c_{DG}$ NSFR.IR-GL-HPF.Smag. $C_{S}=0.10$ $P_{L}=2$", \
     "$c_{DG}$ NSFR.IR-GL-HPF.Smag. $C_{S}=0.10$ $P_{L}=3$", \
     # "$c_{DG}$ NSFR.IR-GL-HPF.Smag. $C_{S}=0.10$ $P_{L}=4$", \
-    "$c_{DG}$ NSFR.IR-GL-SS.VMS $P_{L}=3$", \
+    "$c_{DG}$ NSFR.IR-GL-SI.Smag. $C_{S}=0.10$", \
+    # "$c_{DG}$ NSFR.IR-GL-SS.VMS $P_{L}=3$", \
     ]
     black_line_flag_for_plot=[\
     False,\
@@ -1440,8 +1440,8 @@ if(True):
     dashed_line_flag_for_plot=[\
     False,\
     False,\
-    True,\
     False,\
+    True,\
     False,\
     True,\
     ]
