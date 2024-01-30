ffb3ca743a592923df054238785d0802c6299850
Julien Brillon <43619715+jbrillon@users.noreply.github.com>
2024-01-30 10:15:58 -0500

    passing input file as terminal argument for batch assemble and generate spectra files

--- src/tools/batch_assemble_from_terminal.py
+++ src/tools/batch_assemble_from_terminal.py
@@ -3,5 +3,6 @@ import sys
 sys.path.append(CURRENT_PATH+"../../src/tools");
 from batch_assemble_mpi_flow_field_files_reorder_generate_spectra import batch_assemble_mpi_flow_field_files_reorder_generate_spectra_from_txt
 
-file = input('Specify input file: ')
+file = sys.argv[1] # to pass file from terminal
+print("Specified input file for paths: %s" % file)
 batch_assemble_mpi_flow_field_files_reorder_generate_spectra_from_txt(file)
