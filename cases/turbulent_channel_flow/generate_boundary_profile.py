channel flow work -- computing the Reynolds stresses and fixed the spatial averaging, added Kim's DNS data and compared to that

cases/turbulent_channel_flow/generate_boundary_profile.py
@@ -1,6 +1,7 @@
 #-----------------------------------------------------
 # Import public libraries
 import numpy as np # NumPy: contains basic numerical routines
+from scipy import interpolate
 #-----------------------------------------------------
 # Import personal libraries
 # from finite_difference_library import first_derivative, fd_non_uniform_grid
@@ -41,29 +42,73 @@ def generate_boundary_layer_profile_file_from_flow_field_file(
     vel_data = np.loadtxt(file,skiprows=n_skiprows,usecols=(3,4,5),dtype=np.float64)
     print(" - done.")
     velocity_field = [vel_data[:,0],vel_data[:,1],vel_data[:,2]] # u,v,w
-    print(" - Computing the fluctuating velocity field...")
-    velocity_field = get_fluctuating_velocity_field(velocity_field)
-    print(" - done.")
 
     file_out_without_extension = file_without_extension+"_boundary_layer_profile"
 
-    # NOTE FOR REYNOLDS STRESSES; call get_fluctuating_velocity_field(velocity_field)
     number_of_degrees_of_freedom = np.size(data[:,0])
-    number_of_unique_points_per_direction = number_of_elements_in_y_direction*(poly_degree+1) - (number_of_elements_in_y_direction-1) # total DOFs minus number of interfaces
+    number_of_unique_points_per_y_direction = number_of_elements_in_y_direction*(poly_degree+1) - (number_of_elements_in_y_direction-1) # total DOFs minus number of interfaces
     # (1) compute the expected unique y-stations
-    unique_y_stations = np.linspace(-1.0,1.0,number_of_unique_points_per_direction)
-    number_of_y_stations = np.size(unique_y_stations) # same as number_of_unique_points_per_direction
+    unique_y_stations = np.linspace(-1.0,1.0,number_of_unique_points_per_y_direction)
+    number_of_y_stations = np.size(unique_y_stations) # same as number_of_unique_points_per_y_direction
+    element_interface_coordinates = np.linspace(-1.0,1.0,number_of_elements_in_y_direction+1)
 
-    print(" - Averaging the x-velocity at each y-station...")
+    print(" - Averaging the velocity components at each y-station...")
     # (2) sum all x-velocities and kinematic viscosity at each y-station
     # - x-velocity
     summation_of_xvelocity_at_each_y_station = np.zeros(number_of_y_stations)
     number_of_summations_of_xvelocity_at_each_y_station = np.zeros(number_of_y_stations)
     average_xvelocity_at_each_y_station = np.zeros(number_of_y_stations)
+    # - y-velocity
+    summation_of_yvelocity_at_each_y_station = np.zeros(number_of_y_stations)
+    number_of_summations_of_yvelocity_at_each_y_station = np.zeros(number_of_y_stations)
+    average_yvelocity_at_each_y_station = np.zeros(number_of_y_stations)
+    # - z-velocity
+    summation_of_zvelocity_at_each_y_station = np.zeros(number_of_y_stations)
+    number_of_summations_of_zvelocity_at_each_y_station = np.zeros(number_of_y_stations)
+    average_zvelocity_at_each_y_station = np.zeros(number_of_y_stations)
+    # - x-velocity squared / rms
+    summation_of_xvelocity_squared_at_each_y_station = np.zeros(number_of_y_stations)
+    average_xvelocity_squared_at_each_y_station = np.zeros(number_of_y_stations)
+    rms_xvelocity_at_each_y_station = np.zeros(number_of_y_stations)
     # - kinematic viscosity
     summation_of_kinematic_viscosity_at_each_y_station = np.zeros(number_of_y_stations)
     number_of_summations_of_kinematic_viscosity_at_each_y_station = np.zeros(number_of_y_stations)
     average_kinematic_viscosity_at_each_y_station = np.zeros(number_of_y_stations)
+    for i in range(0,number_of_degrees_of_freedom):
+        # check which y-station
+        y_value = 1.0*data[i,0]
+        index_of_y_station = np.where(np.abs(unique_y_stations-y_value)<1.0e-8)[0][0]
+        # sum the x-velocity
+        velocity_x_direction_value = 1.0*velocity_field[0][i]#data[i,1]
+        summation_of_xvelocity_at_each_y_station[index_of_y_station] += velocity_x_direction_value
+        number_of_summations_of_xvelocity_at_each_y_station[index_of_y_station] += 1.0
+        # sum the y-velocity
+        velocity_y_direction_value = 1.0*velocity_field[1][i]
+        summation_of_yvelocity_at_each_y_station[index_of_y_station] += velocity_y_direction_value
+        number_of_summations_of_yvelocity_at_each_y_station[index_of_y_station] += 1.0
+        # sum the z-velocity
+        velocity_z_direction_value = 1.0*velocity_field[2][i]
+        summation_of_zvelocity_at_each_y_station[index_of_y_station] += velocity_z_direction_value
+        number_of_summations_of_zvelocity_at_each_y_station[index_of_y_station] += 1.0
+        # sum the x-velocity squared
+        velocity_x_squared_direction_value = 1.0*data[i,1]*data[i,1]
+        summation_of_xvelocity_squared_at_each_y_station[index_of_y_station] += velocity_x_squared_direction_value
+        # sum the kinematic viscosity
+        density_value = 1.0*data[i,2]
+        dynamic_viscosity_value = 1.0*data[i,3]
+        kinematic_viscosity_value = dynamic_viscosity_value/density_value
+        summation_of_kinematic_viscosity_at_each_y_station[index_of_y_station] += kinematic_viscosity_value
+        number_of_summations_of_kinematic_viscosity_at_each_y_station[index_of_y_station] += 1.0
+    # (3) compute the average
+    for i in range(0,number_of_y_stations):
+        average_xvelocity_at_each_y_station[i] = summation_of_xvelocity_at_each_y_station[i]/number_of_summations_of_xvelocity_at_each_y_station[i]
+        average_yvelocity_at_each_y_station[i] = summation_of_yvelocity_at_each_y_station[i]/number_of_summations_of_yvelocity_at_each_y_station[i]
+        average_zvelocity_at_each_y_station[i] = summation_of_zvelocity_at_each_y_station[i]/number_of_summations_of_zvelocity_at_each_y_station[i]
+        average_kinematic_viscosity_at_each_y_station[i] = summation_of_kinematic_viscosity_at_each_y_station[i]/number_of_summations_of_kinematic_viscosity_at_each_y_station[i]
+        rms_xvelocity_at_each_y_station[i] = np.sqrt(summation_of_xvelocity_squared_at_each_y_station[i]/number_of_summations_of_xvelocity_at_each_y_station[i])
+    print(" - done.")
+    
+    print(" - Averaging the velocity fluctuation components at each y-station...")
     # - x-velocity fluctuation RMS
     summation_of_xvelocity_fluctuation_rms_at_each_y_station = np.zeros(number_of_y_stations)
     number_of_summations_of_xvelocity_fluctuation_rms_at_each_y_station = np.zeros(number_of_y_stations)
@@ -76,44 +121,71 @@ def generate_boundary_layer_profile_file_from_flow_field_file(
     summation_of_zvelocity_fluctuation_rms_at_each_y_station = np.zeros(number_of_y_stations)
     number_of_summations_of_zvelocity_fluctuation_rms_at_each_y_station = np.zeros(number_of_y_stations)
     average_zvelocity_fluctuation_rms_at_each_y_station = np.zeros(number_of_y_stations)
+    # - Reynolds stress uv
+    summation_of_reynolds_stress_uv_at_each_y_station = np.zeros(number_of_y_stations)
+    number_of_summations_of_reynolds_stress_uv_at_each_y_station = np.zeros(number_of_y_stations)
+    average_reynolds_stress_uv_at_each_y_station = np.zeros(number_of_y_stations)
     for i in range(0,number_of_degrees_of_freedom):
         # check which y-station
         y_value = 1.0*data[i,0]
         index_of_y_station = np.where(np.abs(unique_y_stations-y_value)<1.0e-8)[0][0]
-        # sum the x-velocity
-        velocity_x_direction_value = 1.0*data[i,1]
-        summation_of_xvelocity_at_each_y_station[index_of_y_station] += velocity_x_direction_value
-        number_of_summations_of_xvelocity_at_each_y_station[index_of_y_station] += 1.0
-        # sum the kinematic viscosity
-        density_value = 1.0*data[i,2]
-        dynamic_viscosity_value = 1.0*data[i,3]
-        kinematic_viscosity_value = dynamic_viscosity_value/density_value
-        summation_of_kinematic_viscosity_at_each_y_station[index_of_y_station] += kinematic_viscosity_value
-        number_of_summations_of_kinematic_viscosity_at_each_y_station[index_of_y_station] += 1.0
         # sum the x-velocity fluctuation RMS
-        velocity_x_direction_fluctuation_value = 1.0*velocity_field[0][i]
-        summation_of_xvelocity_fluctuation_rms_at_each_y_station[index_of_y_station] = velocity_x_direction_fluctuation_value*velocity_x_direction_fluctuation_value
+        velocity_x_direction_fluctuation_value = 1.0*velocity_field[0][i]-average_xvelocity_at_each_y_station[index_of_y_station]
+        summation_of_xvelocity_fluctuation_rms_at_each_y_station[index_of_y_station] += velocity_x_direction_fluctuation_value*velocity_x_direction_fluctuation_value
         number_of_summations_of_xvelocity_fluctuation_rms_at_each_y_station[index_of_y_station] += 1.0
         # sum the y-velocity fluctuation RMS
-        velocity_y_direction_fluctuation_value = 1.0*velocity_field[1][i]
-        summation_of_yvelocity_fluctuation_rms_at_each_y_station[index_of_y_station] = velocity_y_direction_fluctuation_value*velocity_y_direction_fluctuation_value
+        velocity_y_direction_fluctuation_value = 1.0*velocity_field[1][i]-average_yvelocity_at_each_y_station[index_of_y_station]
+        summation_of_yvelocity_fluctuation_rms_at_each_y_station[index_of_y_station] += velocity_y_direction_fluctuation_value*velocity_y_direction_fluctuation_value
         number_of_summations_of_yvelocity_fluctuation_rms_at_each_y_station[index_of_y_station] += 1.0
         # sum the z-velocity fluctuation RMS
-        velocity_z_direction_fluctuation_value = 1.0*velocity_field[2][i]
-        summation_of_zvelocity_fluctuation_rms_at_each_y_station[index_of_y_station] = velocity_z_direction_fluctuation_value*velocity_z_direction_fluctuation_value
+        velocity_z_direction_fluctuation_value = 1.0*velocity_field[2][i]-average_zvelocity_at_each_y_station[index_of_y_station]
+        summation_of_zvelocity_fluctuation_rms_at_each_y_station[index_of_y_station] += velocity_z_direction_fluctuation_value*velocity_z_direction_fluctuation_value
         number_of_summations_of_zvelocity_fluctuation_rms_at_each_y_station[index_of_y_station] += 1.0
-    # (3) compute the average
+        # sum the reynolds stress u'v'
+        reynolds_stress_uv_value = velocity_x_direction_fluctuation_value*velocity_y_direction_fluctuation_value
+        summation_of_reynolds_stress_uv_at_each_y_station[index_of_y_station] += reynolds_stress_uv_value
+        number_of_summations_of_reynolds_stress_uv_at_each_y_station[index_of_y_station] += 1.0
+    # compute the average
     for i in range(0,number_of_y_stations):
-        average_xvelocity_at_each_y_station[i] = summation_of_xvelocity_at_each_y_station[i]/number_of_summations_of_xvelocity_at_each_y_station[i]
-        average_kinematic_viscosity_at_each_y_station[i] = summation_of_kinematic_viscosity_at_each_y_station[i]/number_of_summations_of_kinematic_viscosity_at_each_y_station[i]
         average_xvelocity_fluctuation_rms_at_each_y_station[i] = np.sqrt(summation_of_xvelocity_fluctuation_rms_at_each_y_station[i]/number_of_summations_of_xvelocity_fluctuation_rms_at_each_y_station[i])
         average_yvelocity_fluctuation_rms_at_each_y_station[i] = np.sqrt(summation_of_yvelocity_fluctuation_rms_at_each_y_station[i]/number_of_summations_of_yvelocity_fluctuation_rms_at_each_y_station[i])
         average_zvelocity_fluctuation_rms_at_each_y_station[i] = np.sqrt(summation_of_zvelocity_fluctuation_rms_at_each_y_station[i]/number_of_summations_of_zvelocity_fluctuation_rms_at_each_y_station[i])
+        average_reynolds_stress_uv_at_each_y_station[i] = summation_of_reynolds_stress_uv_at_each_y_station[i]/number_of_summations_of_reynolds_stress_uv_at_each_y_station[i]
     print(" - done.")
+
+    # Fix element interfaces by interpolating between neighbouring points
+    print(" Fixing element interfaces...")
+    i_interface=0
+    print(element_interface_coordinates)
+    # np.abs(y_plus - y_plus_wall_model_input)<1e-4)
+    for i in range(0,number_of_y_stations):
+        if(np.abs(unique_y_stations[i]-element_interface_coordinates[i_interface])<1.0e-4):
+            # note: for wall model, we dont care about the wall boundary point, and because of symmetry with the channel flow, we dont care about the right boundary point (i.e. ymax)
+            # print("at an interface")
+            # print(unique_y_stations[i])
+            # print(element_interface_coordinates[i_interface])
+            if(i_interface>0 and i_interface<number_of_elements_in_y_direction):
+                # print("    at element %i interface..." % i_interface)
+                average_xvelocity_at_each_y_station[i] = 0.5*(average_xvelocity_at_each_y_station[i+1]+average_xvelocity_at_each_y_station[i-1])
+                average_yvelocity_at_each_y_station[i] = 0.5*(average_yvelocity_at_each_y_station[i+1]+average_yvelocity_at_each_y_station[i-1])
+                average_zvelocity_at_each_y_station[i] = 0.5*(average_zvelocity_at_each_y_station[i+1]+average_zvelocity_at_each_y_station[i-1])
+                average_kinematic_viscosity_at_each_y_station[i] = 0.5*(average_kinematic_viscosity_at_each_y_station[i+1]+average_kinematic_viscosity_at_each_y_station[i-1])
+                rms_xvelocity_at_each_y_station[i] = 0.5*(rms_xvelocity_at_each_y_station[i+1]+rms_xvelocity_at_each_y_station[i-1])
+                print(average_xvelocity_fluctuation_rms_at_each_y_station[i])
+                average_xvelocity_fluctuation_rms_at_each_y_station[i] = 0.5*(average_xvelocity_fluctuation_rms_at_each_y_station[i+1]+average_xvelocity_fluctuation_rms_at_each_y_station[i-1])
+                print("new averaged value:")
+                print(average_xvelocity_fluctuation_rms_at_each_y_station[i])
+                average_yvelocity_fluctuation_rms_at_each_y_station[i] = 0.5*(average_yvelocity_fluctuation_rms_at_each_y_station[i+1]+average_yvelocity_fluctuation_rms_at_each_y_station[i-1])
+                average_zvelocity_fluctuation_rms_at_each_y_station[i] = 0.5*(average_zvelocity_fluctuation_rms_at_each_y_station[i+1]+average_zvelocity_fluctuation_rms_at_each_y_station[i-1])
+                average_reynolds_stress_uv_at_each_y_station[i] = 0.5*(average_reynolds_stress_uv_at_each_y_station[i+1]+average_reynolds_stress_uv_at_each_y_station[i-1])
+            # update interface
+            i_interface += 1
+    print(" - done.")
+
     # (4) output file for the boundary layer profile    
     file_out = file_out_without_extension+"."+file_extension
     print(" - Writing file: %s" % file_out)
-    np.savetxt(file_out,np.transpose(np.array([unique_y_stations, average_xvelocity_at_each_y_station, average_kinematic_viscosity_at_each_y_station, average_xvelocity_fluctuation_rms_at_each_y_station, average_yvelocity_fluctuation_rms_at_each_y_station, average_zvelocity_fluctuation_rms_at_each_y_station])))
+    np.savetxt(file_out,np.transpose(np.array([unique_y_stations, average_xvelocity_at_each_y_station, average_kinematic_viscosity_at_each_y_station, average_xvelocity_fluctuation_rms_at_each_y_station, average_yvelocity_fluctuation_rms_at_each_y_station, average_zvelocity_fluctuation_rms_at_each_y_station, average_reynolds_stress_uv_at_each_y_station])))
     print(" - done.")
 
     print("done.")

cases/turbulent_channel_flow/plot_flow_convergence.py
@@ -113,7 +113,11 @@ def plot_boundary_layer_profile(filenames_,labels_,friction_velocity_based_reyno
     # data store
     average_y_plus_store=[]
     average_u_plus_store=[]
-    average_velocity_fluctuation_rms_store=[]
+    average_x_velocity_fluctuation_rms_store=[]
+    average_y_velocity_fluctuation_rms_store=[]
+    average_z_velocity_fluctuation_rms_store=[]
+    average_reynolds_stress_uv_store=[]
+    nondim_y_store = []
     # plot function inputs
     labels_store=[]
 
@@ -136,16 +140,25 @@ def plot_boundary_layer_profile(filenames_,labels_,friction_velocity_based_reyno
         average_u_plus_store.append(average_u_plus_reference)
         labels_store.append("p4 DG-WMLES\n [Fr\\`ere et al., 2017]")
     elif(friction_velocity_based_reynolds_number==395):
-        # load reference data
-        y_plus_reference, average_u_plus_reference = np.loadtxt("./data/reference/moser_et_al_1999_dns_Re395.txt",skiprows=1,dtype=np.float64,unpack=True,delimiter=",")
+        # # load reference data
+        # y_plus_reference, average_u_plus_reference = np.loadtxt("./data/reference/moser_et_al_1999_dns_Re395.txt",skiprows=1,dtype=np.float64,unpack=True,delimiter=",")
+        # average_y_plus_store.append(y_plus_reference)
+        # average_u_plus_store.append(average_u_plus_reference)
+        # labels_store.append("DNS [Moser et al., 1999]")
+        # load Kim data; available at http://cfd.mace.manchester.ac.uk/ercoftac/doku.php?id=cases:case032
+        nondim_y, y_plus_reference, average_u_plus_reference, uuplus, vvplus, wwplus, uvplus = np.loadtxt("./data/reference/kim_1989_data_Re395.txt",usecols=(1,2,3,4,5,6,7),skiprows=11,max_rows=97,dtype=np.float64,unpack=True)
         average_y_plus_store.append(y_plus_reference)
         average_u_plus_store.append(average_u_plus_reference)
-        labels_store.append("DNS [Moser et al., 1999]")#\n$(\\Delta x^{+},\\Delta y_{c}^{+},\\Delta y_{w}^{+},\\Delta z^{+})=(?,?,?,?)$")
-
+        nondim_y_store.append(nondim_y)
+        average_x_velocity_fluctuation_rms_store.append(np.sqrt(uuplus))
+        average_y_velocity_fluctuation_rms_store.append(np.sqrt(vvplus))
+        average_z_velocity_fluctuation_rms_store.append(np.sqrt(wwplus))
+        average_reynolds_stress_uv_store.append(-uvplus)
+        labels_store.append("DNS [Lee, 1989]")
 
     for i,filename in enumerate(filenames_):
         # load data
-        y_coordinate, average_x_velocity, average_kinematic_viscosity, average_x_velocity_fluctuation_rms, average_y_velocity_fluctuation_rms, average_z_velocity_fluctuation_rms = np.loadtxt(filename,dtype=np.float64,unpack=True)
+        y_coordinate, average_x_velocity, average_kinematic_viscosity, average_x_velocity_fluctuation_rms, average_y_velocity_fluctuation_rms, average_z_velocity_fluctuation_rms, average_reynolds_stress_uv = np.loadtxt(filename,dtype=np.float64,unpack=True)
 
         number_of_points = np.size(y_coordinate)
         y_plus = []
@@ -153,6 +166,7 @@ def plot_boundary_layer_profile(filenames_,labels_,friction_velocity_based_reyno
         u_plus_fluctuation = []
         v_plus_fluctuation = []
         w_plus_fluctuation = []
+        reynolds_stress_uv = []
         for j in range(0,number_of_points):
             y_value = y_coordinate[j] # nondimensional
             distance_from_wall = 0.0 # nondimensional
@@ -185,13 +199,16 @@ def plot_boundary_layer_profile(filenames_,labels_,friction_velocity_based_reyno
             u_plus_fluctuation.append(u_plus_fluctuation_local)
             v_plus_fluctuation.append(v_plus_fluctuation_local)
             w_plus_fluctuation.append(w_plus_fluctuation_local)
+            # Reynolds stresses
+            reynolds_stress_uv_local = np.abs(average_reynolds_stress_uv[j])/nondimensional_friction_velocity/nondimensional_friction_velocity
+            reynolds_stress_uv.append(reynolds_stress_uv_local)
 
         y_plus = np.array(y_plus)
         u_plus = np.array(u_plus)
         u_plus_fluctuation = np.array(u_plus_fluctuation)
         v_plus_fluctuation = np.array(v_plus_fluctuation)
         w_plus_fluctuation = np.array(w_plus_fluctuation)
-
+        reynolds_stress_uv = np.array(reynolds_stress_uv)
         number_of_unique_BL_points = int(0.5*np.size(y_plus))+1
         index_centerline = number_of_unique_BL_points - 1
         unique_y_plus=np.zeros(number_of_unique_BL_points)
@@ -199,11 +216,13 @@ def plot_boundary_layer_profile(filenames_,labels_,friction_velocity_based_reyno
         unique_u_plus_fluctuation=np.zeros(number_of_unique_BL_points)
         unique_v_plus_fluctuation=np.zeros(number_of_unique_BL_points)
         unique_w_plus_fluctuation=np.zeros(number_of_unique_BL_points)
+        unique_reynolds_stress_uv=np.zeros(number_of_unique_BL_points)
         unique_y_plus[index_centerline] = 1.0*y_plus[index_centerline]
         unique_u_plus[index_centerline] = 1.0*u_plus[index_centerline]
         unique_u_plus_fluctuation[index_centerline] = 1.0*u_plus_fluctuation[index_centerline]
         unique_v_plus_fluctuation[index_centerline] = 1.0*v_plus_fluctuation[index_centerline]
         unique_w_plus_fluctuation[index_centerline] = 1.0*w_plus_fluctuation[index_centerline]
+        unique_reynolds_stress_uv[index_centerline] = 1.0*reynolds_stress_uv[index_centerline]
         for j in range(0,index_centerline):
             unique_y_plus[j] = y_plus[j]
             index_for_second_BL = -(j+1)
@@ -219,6 +238,7 @@ def plot_boundary_layer_profile(filenames_,labels_,friction_velocity_based_reyno
             unique_u_plus_fluctuation[j] = 0.5*(u_plus_fluctuation[j]+u_plus_fluctuation[index_for_second_BL])
             unique_v_plus_fluctuation[j] = 0.5*(v_plus_fluctuation[j]+v_plus_fluctuation[index_for_second_BL])
             unique_w_plus_fluctuation[j] = 0.5*(w_plus_fluctuation[j]+w_plus_fluctuation[index_for_second_BL])
+            unique_reynolds_stress_uv[j] = 0.5*(reynolds_stress_uv[j]+reynolds_stress_uv[index_for_second_BL])
 
         # marker for the input
         y_plus_wall_model_input = 0.2*Re_tau # 0.2 is the uniform delta y from the grid
@@ -231,16 +251,19 @@ def plot_boundary_layer_profile(filenames_,labels_,friction_velocity_based_reyno
         u_plus_fluctuation_after_wall_model_input_point = unique_u_plus_fluctuation[np.where(unique_y_plus >= (y_plus_wall_model_input-1e-4))]
         v_plus_fluctuation_after_wall_model_input_point = unique_v_plus_fluctuation[np.where(unique_y_plus >= (y_plus_wall_model_input-1e-4))]
         w_plus_fluctuation_after_wall_model_input_point = unique_w_plus_fluctuation[np.where(unique_y_plus >= (y_plus_wall_model_input-1e-4))]
-        
+        reynolds_stress_uv_after_wall_model_input_point = unique_reynolds_stress_uv[np.where(unique_y_plus >= (y_plus_wall_model_input-1e-4))]
+
         # add to plot
         average_y_plus_store.append(y_plus_after_wall_model_input_point)
         labels_store.append(labels_[i])
         average_u_plus_store.append(u_plus_after_wall_model_input_point)
 
-        average_velocity_fluctuation_rms_store.append(u_plus_fluctuation_after_wall_model_input_point)
-        average_velocity_fluctuation_rms_store.append(v_plus_fluctuation_after_wall_model_input_point)
-        average_velocity_fluctuation_rms_store.append(w_plus_fluctuation_after_wall_model_input_point)
+        average_x_velocity_fluctuation_rms_store.append(u_plus_fluctuation_after_wall_model_input_point)
+        average_y_velocity_fluctuation_rms_store.append(v_plus_fluctuation_after_wall_model_input_point)
+        average_z_velocity_fluctuation_rms_store.append(w_plus_fluctuation_after_wall_model_input_point)
 
+        average_reynolds_stress_uv_store.append(reynolds_stress_uv_after_wall_model_input_point)
+        nondim_y_store.append(y_plus_after_wall_model_input_point/np.float64(friction_velocity_based_reynolds_number))
         # add the marker to the plot for wall model input location -- can uncomment to add the marker
         # average_y_plus_store.append(np.array(y_plus_wall_model_input))
         # labels_store.append("Wall Model Input")
@@ -257,7 +280,7 @@ def plot_boundary_layer_profile(filenames_,labels_,friction_velocity_based_reyno
         which_lines_black_=[0]
         which_lines_only_markers_=[]
         which_lines_dashed_=[]
-    
+
     qp.plotfxn(average_y_plus_store,average_u_plus_store,
         figure_filename="boundary_layer_profile_Re"+str(friction_velocity_based_reynolds_number),
         figure_size=(7,6),
@@ -306,20 +329,41 @@ def plot_boundary_layer_profile(filenames_,labels_,friction_velocity_based_reyno
         legend_location="upper left",
         vertical_lines=[y_plus_wall_model_input])
 
-    # fluctuations
-    xdata_for_fluctuations=[y_plus_after_wall_model_input_point,y_plus_after_wall_model_input_point,y_plus_after_wall_model_input_point]
-    labels_store = ["$\\left\\langle u'^{+}\\right\\rangle _{rms}$",\
-                    "$\\left\\langle v'^{+}\\right\\rangle _{rms}$",\
-                    "$\\left\\langle w'^{+}\\right\\rangle _{rms}$"]
-    qp.plotfxn(xdata_for_fluctuations,average_velocity_fluctuation_rms_store,
-        figure_filename="boundary_layer_profile_of_fluctuations_Re"+str(friction_velocity_based_reynolds_number),
+    qp.plotfxn(nondim_y_store,average_reynolds_stress_uv_store,
+        figure_filename="reynolds_stress_uv_profile_Re"+str(friction_velocity_based_reynolds_number),
         figure_size=(7,6),
         legend_labels_tex=labels_store,
         figure_filetype="pdf",
         # title_label="Turbulent Channel Flow $Re_{\\tau}\\approx395$, $CFL\\approx0.2$, $\\alpha=0.0$",
-        title_label="WMLES Approach to Turbulent Channel Flow at $Re_{\\tau}\\approx5200$",
+        # title_label="WMLES Approach to Turbulent Channel Flow at $Re_{\\tau}\\approx5200$",
+        # xlabel="$\\left\\langle y^{+}\\right\\rangle$",
+        xlabel="$y/\\delta$",
+        ylabel="$-u'v'/u_{\\tau}^{2}$",
+        # xlimits=[],
+        # ylimits=ylimits_,
+        which_lines_black=which_lines_black_,
+        which_lines_only_markers=which_lines_only_markers_,
+        transparent_legend=True,
+        legend_border_on=False,
+        grid_lines_on=False,
+        log_axes=None,
+        legend_location="best",
+        vertical_lines=[y_plus_wall_model_input/np.float64(friction_velocity_based_reynolds_number)])
+
+    # fluctuations
+    # xdata_for_fluctuations=[y_plus_after_wall_model_input_point,y_plus_after_wall_model_input_point,y_plus_after_wall_model_input_point]
+    # labels_store = ["$\\left\\langle u'^{+}\\right\\rangle _{rms}$",\
+    #                 "$\\left\\langle v'^{+}\\right\\rangle _{rms}$",\
+    #                 "$\\left\\langle w'^{+}\\right\\rangle _{rms}$"]
+    qp.plotfxn(nondim_y_store,average_x_velocity_fluctuation_rms_store,
+        figure_filename="boundary_layer_profile_of_x_velocity_fluctuations_Re"+str(friction_velocity_based_reynolds_number),
+        figure_size=(7,6),
+        legend_labels_tex=labels_store,
+        figure_filetype="pdf",
+        # title_label="Turbulent Channel Flow $Re_{\\tau}\\approx395$, $CFL\\approx0.2$, $\\alpha=0.0$",
+        # title_label="WMLES Approach to Turbulent Channel Flow at $Re_{\\tau}\\approx395$",
         xlabel="$\\left\\langle y^{+}\\right\\rangle$",
-        ylabel="$\\left\\langle u'^{+}_{i}\\right\\rangle _{rms}$",
+        ylabel="$\\left\\langle u'^{+}\\right\\rangle _{rms}$",
         xlimits=xlimits_,
         ylimits=[],
         which_lines_black=[],
@@ -329,7 +373,47 @@ def plot_boundary_layer_profile(filenames_,labels_,friction_velocity_based_reyno
         grid_lines_on=True,
         log_axes=None,
         legend_location="best",
-        vertical_lines=[y_plus_wall_model_input])
+        vertical_lines=[y_plus_wall_model_input/np.float64(friction_velocity_based_reynolds_number)])
+    qp.plotfxn(nondim_y_store,average_y_velocity_fluctuation_rms_store,
+        figure_filename="boundary_layer_profile_of_y_velocity_fluctuations_Re"+str(friction_velocity_based_reynolds_number),
+        figure_size=(7,6),
+        legend_labels_tex=labels_store,
+        figure_filetype="pdf",
+        # title_label="Turbulent Channel Flow $Re_{\\tau}\\approx395$, $CFL\\approx0.2$, $\\alpha=0.0$",
+        # title_label="WMLES Approach to Turbulent Channel Flow at $Re_{\\tau}\\approx395$",
+        xlabel="$\\left\\langle y^{+}\\right\\rangle$",
+        ylabel="$\\left\\langle v'^{+}\\right\\rangle _{rms}$",
+        xlimits=xlimits_,
+        ylimits=[],
+        which_lines_black=[],
+        which_lines_only_markers=[],
+        transparent_legend=False,
+        legend_border_on=True,
+        grid_lines_on=True,
+        log_axes=None,
+        legend_location="best",
+        vertical_lines=[y_plus_wall_model_input/np.float64(friction_velocity_based_reynolds_number)])
+    qp.plotfxn(nondim_y_store,average_z_velocity_fluctuation_rms_store,
+        figure_filename="boundary_layer_profile_of_z_velocity_fluctuations_Re"+str(friction_velocity_based_reynolds_number),
+        figure_size=(7,6),
+        legend_labels_tex=labels_store,
+        figure_filetype="pdf",
+        # title_label="Turbulent Channel Flow $Re_{\\tau}\\approx395$, $CFL\\approx0.2$, $\\alpha=0.0$",
+        # title_label="WMLES Approach to Turbulent Channel Flow at $Re_{\\tau}\\approx395$",
+        xlabel="$\\left\\langle y^{+}\\right\\rangle$",
+        ylabel="$\\left\\langle w'^{+}\\right\\rangle _{rms}$",
+        xlimits=xlimits_,
+        ylimits=[],
+        which_lines_black=[],
+        which_lines_only_markers=[],
+        transparent_legend=False,
+        legend_border_on=True,
+        grid_lines_on=True,
+        log_axes=None,
+        legend_location="best",
+        vertical_lines=[y_plus_wall_model_input/np.float64(friction_velocity_based_reynolds_number)])
+
+
     return
 #-----------------------------------------------------
 #=====================================================
@@ -403,6 +487,16 @@ which_lines_dashed=[]
 friction_velocity_based_reynolds_number=[5200,395]
 plot_transient(filenames,labels,starting_data_index_for_plot=0,friction_velocity_based_reynolds_number=friction_velocity_based_reynolds_number)
 # exit()
+
+filenames=[\
+filesystem+"NarvalFiles/2024_AIAA/turbulent_channel_flow/viscous_TCF_ILES_NSFR_cDG_IR_2PF_GLL_OI-0_Re395_p4_20x10x10_turbulent_initialization/flow_field_files/velocity_vorticity-0_boundary_layer_profile_t0300.dat",\
+]
+labels=[\
+"P$4$ NSFR-WMLES $(t^{*}=300)$\n $n_{x}\\times n_{y}\\times n_{z} = 20\\times 10\\times 10$",\
+]
+which_lines_dashed=[]
+plot_boundary_layer_profile(filenames,labels,395)
+exit()
 # plot boundary layer profile
 filenames=[\
 # filesystem+"NarvalFiles/2024_AIAA/turbulent_channel_flow/viscous_TCF_ILES_NSFR_cDG_IR_2PF_GLL_OI-0_Re5200_p4_20x10x10_turbulent_initialization/flow_field_files/velocity_vorticity-0_boundary_layer_profile_t0200.dat",\