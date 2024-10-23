4379098bce677e79f185225bd5a9dac828f1d737
jbrillon <43619715+jbrillon@users.noreply.github.com>
2024-10-23 15:39:22 -0400

    cleaning up DWC contour plots

--- cases/dipole_wall_collision/plot_paraview_data_slice.py
+++ cases/dipole_wall_collision/plot_paraview_data_slice.py
@@ -16,6 +16,11 @@ import matplotlib.tri as tri
 
 from struct import unpack # for loading the reference data
 #-------------------------------------------------------------
+def get_reference_result():
+    path = filesystem+"NarvalFiles/2024_CSME/dipole_wall_collision/viscous_DWC_ILES_NSFR_cDG_IR_2PF_GL_OI-0_dofs1024_p7/solution_files/"
+    filename = path+"vorticity"+"."+"csv"
+    return get_paraview_file_data(filename)
+#-------------------------------------------------------------
 def get_paraview_file_data(
     input_filename,
     scalar_field_name="vorticity_magnitude"):
@@ -29,7 +34,7 @@ def get_paraview_file_data(
 def plot_vorticity_plane(path,filename_without_extension,file_extension,fig_directory,fig_prepre_fix,
     subdivide=False,title_label=" ",fill_contour=False,plot_reference_result=False,
     shift_domain_by_minus_pi=False,plot_single_element_domain=False,nElements_per_direction=16,
-    plot_at_wall=False,color_bar_off=False,plot_wall_element_edge=True,wall_element_size_dx=0.1):
+    plot_at_wall=False,color_bar_off=False,plot_wall_element_edge=False,wall_element_size_dx=0.0):
     # TO DO: move this somewhere else eventually
     figure_filename = fig_prepre_fix+filename_without_extension
     if(subdivide):
@@ -92,7 +97,11 @@ def plot_vorticity_plane(path,filename_without_extension,file_extension,fig_dire
     #     contour_levels = [1, 5, 10, 20, 30]
     # else:
     #     contour_levels = np.linspace(1,30,20)
-    contour_levels = np.linspace(scalar_min,scalar_max,6)
+    if(fill_contour):
+        contour_levels = np.linspace(scalar_min,scalar_max,6)
+    else:
+        contour_levels = np.arange(10.0,271.0,20.0)
+        # contour_levels = np.linspace(scalar_min,scalar_max,6)
 
     # Figure parameters
     # fig_directory = "figures"
@@ -128,8 +137,8 @@ def plot_vorticity_plane(path,filename_without_extension,file_extension,fig_dire
     #     plt.ylim([-np.pi,0.0])
 
     if(plot_at_wall):
-        plt.ylim([0.0,0.4])
-        plt.xlim([0.6,1.0])
+        plt.ylim([0.0,0.5])
+        plt.xlim([0.5,1.0])
 
     ax.set_xlabel(x_label,fontsize=axisTitle_FontSize)
     ax.set_ylabel(y_label,rotation=90,fontsize=axisTitle_FontSize)
@@ -137,21 +146,27 @@ def plot_vorticity_plane(path,filename_without_extension,file_extension,fig_dire
         plt.title(title_label,fontsize=2*axisTitle_FontSize)
     plt.setp(ax.get_xticklabels(),fontsize=axisTickLabel_FontSize); plt.setp(ax.get_yticklabels(),fontsize=axisTickLabel_FontSize);
 
+    if(plot_reference_result):
+        [x,y,w] = get_reference_result()
+        plt.tricontour(x,y,w,levels=contour_levels,colors=('k',),linewidths=(1.0,),linestyles=("dashed",))
+        # plt.contour(x,y,w, levels = contour_levels, colors=('k',),linewidths=(1,))
+
+    if(plot_reference_result):
+        contour_line_clr_for_result = 'r'
+    else:
+        contour_line_clr_for_result = 'k'
+
     if(subdivide):
         if(fill_contour):
             cs = plt.tricontourf(tri_refi, z_test_refi, np.linspace(scalar_min,scalar_max,100),cmap='rainbow',extend="both")
-        if(plot_reference_result):
-            contour_line_clr_for_result = 'r'
-        else:
-            contour_line_clr_for_result = 'k'
-        cs_lines = plt.tricontour(tri_refi,z_test_refi,levels=contour_levels,colors=(contour_line_clr_for_result,),linewidths=(0.0,)) # change to 1
+        cs_lines = plt.tricontour(tri_refi,z_test_refi,levels=contour_levels,colors=(contour_line_clr_for_result,),linewidths=(0.5,)) # change to 1
     else:
         if(fill_contour):
             # cs = plt.tricontourf(X, Y, Z, np.linspace(np.amin(Z),np.amax(Z),100),cmap='rainbow')
             cs = plt.tricontourf(X, Y, Z, np.linspace(scalar_min,scalar_max,100),cmap='rainbow',extend="both")
             # cs = plt.tricontourf(X, Y, Z, np.linspace(0.0,32,100),cmap='rainbow')
-        cs_lines = plt.tricontour(X,Y,Z,levels=contour_levels,colors=('k',),linewidths=(0.0,))
-
+        cs_lines = plt.tricontour(X,Y,Z,levels=contour_levels,colors=(contour_line_clr_for_result,),linewidths=(0.5,))
+    '''
     for level in cs_lines.collections:
         for kp,lpath in reversed(list(enumerate(level.get_paths()))):
             # go in reversed order due to deletions!
@@ -162,9 +177,9 @@ def plot_vorticity_plane(path,filename_without_extension,file_extension,fig_dire
             verts = lpath.vertices # (N,2)-shape array of contour line coordinates
             diameter = np.max(verts.max(axis=0) - verts.min(axis=0))
 
-            if diameter<0.15: # threshold to be refined for your actual dimensions!
+            if diameter<0.035: # threshold to be refined for your actual dimensions!
                 del(level.get_paths()[kp])  # no remove() for Path objects:(
-
+    '''
     if(fill_contour):
         cbar = fig.colorbar(cs,ticks=contour_levels)
         # cbar = fig.colorbar(cs,ticks=np.linspace(np.amin(Z),np.amax(Z),8))
@@ -176,11 +191,7 @@ def plot_vorticity_plane(path,filename_without_extension,file_extension,fig_dire
         for c in cs.collections:
             c.set_edgecolor("face")
 
-    if(plot_reference_result):
-        [x,y,w] = get_reference_result()
-        plt.contour(x,y,w, levels = contour_levels, colors=('k',),linewidths=(1,))
-
-    if(plot_at_wall or color_bar_off):
+    if((plot_at_wall or color_bar_off) and (fill_contour==True)):
         cbar.remove()
     if(plot_wall_element_edge):
         ax.axvline(x=(1.0-wall_element_size_dx),linestyle="dashed",color="k",alpha=0.5)
@@ -192,79 +203,88 @@ def plot_vorticity_plane(path,filename_without_extension,file_extension,fig_dire
     print("---------------------------------------------")
     return
 
-# compare solution at wall
-paths=(
-filesystem+"NarvalFiles/2024_CSME/dipole_wall_collision/viscous_DWC_ILES_NSFR_cDG_IR_2PF_GL_OI-0_dofs0192_p2/solution_files/",\
-filesystem+"NarvalFiles/2024_CSME/dipole_wall_collision/viscous_DWC_ILES_NSFR_cDG_IR_2PF_GL_OI-0_dofs0384_p5/solution_files/",\
-filesystem+"NarvalFiles/2024_CSME/dipole_wall_collision/viscous_DWC_ILES_NSFR_cDG_IR_2PF_GL_OI-0_dofs0512_p7/solution_files/",\
-filesystem+"NarvalFiles/2024_CSME/dipole_wall_collision/viscous_DWC_ILES_NSFR_cDG_IR_2PF_GL_OI-0_dofs1024_p7/solution_files/",\
-filesystem+"NarvalFiles/2024_CSME/dipole_wall_collision/viscous_DWC_ILES_NSFR_cDG_IR_2PF_GL_OI-0_dofs0192_p2_stretched_mesh/solution_files/",\
-)
-file_extension="csv"
-n_paths=len(paths)
-fig_directory = "./figures/2024_CSME"
-labels_for_plot=[\
-    "$192^2$ P$2$ Uniform Mesh",\
-    "$384^2$ P$5$ Uniform Mesh",\
-    "$512^2$ P$7$ Uniform Mesh",\
-    "$1024^2$ P$7$ Uniform Mesh",\
-    "$192^2$ P$2$ Stretched Mesh",\
-]
-fig_prepre_fix = [\
-    "t4_192_p2_NSFR_cDG_IR_GL_",\
-    "t4_384_p5_NSFR_cDG_IR_GL_",\
-    "t4_512_p7_NSFR_cDG_IR_GL_",\
-    "t4_1024_p7_NSFR_cDG_IR_GL_",\
-    "t4_192_p2_stretched_mesh_NSFR_cDG_IR_GL_",\
-]
-subdivide_input=[True,False,False,False,True]
-# subdivide_input=[True,True,True,False,True]
-time = [0.0,0.1,0.2,0.3,0.35,0.5,0.6,0.8,1.0]
-wall_element_size_dx_store=[\
-    (2.0/64.0),\
-    (2.0/64.0),\
-    (2.0/64.0),\
-    (2.0/128.0),\
-    (1.0-9.9879544973373413e-01),\
-]
-for i in range(0,n_paths):
-    filename_without_extension = "vorticity"
-    plot_title = labels_for_plot[i] #"$t=%1.2f$" % time[4]
-    plot_vorticity_plane(paths[i],filename_without_extension,file_extension,fig_directory,fig_prepre_fix[i],
-        subdivide=subdivide_input[i],title_label=plot_title,fill_contour=True,plot_reference_result=False,
-        shift_domain_by_minus_pi=False,
-        plot_at_wall=True,
-        plot_single_element_domain=False,
-        plot_wall_element_edge=True,wall_element_size_dx=wall_element_size_dx_store[i])
-exit()
-# simulation overview
-paths=(
-# filesystem+"NarvalFiles/2024_CSME/dipole_wall_collision/viscous_DWC_ILES_NSFR_cDG_IR_2PF_GL_OI-0_dofs0384_p5/solution_files/",\
-filesystem+"NarvalFiles/2024_CSME/dipole_wall_collision/viscous_DWC_ILES_NSFR_cDG_IR_2PF_GL_OI-0_dofs0512_p7/solution_files/",\
-)
-files_per_path=[9,9]
+if(True):
+    # compare solution at wall
+    paths=(
+    filesystem+"NarvalFiles/2024_CSME/dipole_wall_collision/viscous_DWC_ILES_NSFR_cDG_IR_2PF_GL_OI-0_dofs0192_p2/solution_files/",\
+    filesystem+"NarvalFiles/2024_CSME/dipole_wall_collision/viscous_DWC_ILES_NSFR_cDG_IR_2PF_GL_OI-0_dofs0384_p5/solution_files/",\
+    filesystem+"NarvalFiles/2024_CSME/dipole_wall_collision/viscous_DWC_ILES_NSFR_cDG_IR_2PF_GL_OI-0_dofs0512_p7/solution_files/",\
+    filesystem+"NarvalFiles/2024_CSME/dipole_wall_collision/viscous_DWC_ILES_NSFR_cDG_IR_2PF_GL_OI-0_dofs1024_p7/solution_files/",\
+    filesystem+"NarvalFiles/2024_CSME/dipole_wall_collision/viscous_DWC_ILES_NSFR_cDG_IR_2PF_GL_OI-0_dofs0192_p2_stretched_mesh/solution_files/",\
+    )
+    file_extension="csv"
+    n_paths=len(paths)
+    fig_directory = "./figures/2024_CSME"
+    labels_for_plot=[\
+        "$192^2$ P$2$ Uniform Mesh",\
+        "$384^2$ P$5$ Uniform Mesh",\
+        "$512^2$ P$7$ Uniform Mesh",\
+        "$1024^2$ P$7$ Uniform Mesh",\
+        "$192^2$ P$2$ Stretched Mesh",\
+    ]
+    fig_prepre_fix = [\
+        "t4_192_p2_NSFR_cDG_IR_GL_",\
+        "t4_384_p5_NSFR_cDG_IR_GL_",\
+        "t4_512_p7_NSFR_cDG_IR_GL_",\
+        "t4_1024_p7_NSFR_cDG_IR_GL_",\
+        "t4_192_p2_stretched_mesh_NSFR_cDG_IR_GL_",\
+    ]
+    subdivide_input=[True,False,False,False,True]
+    # subdivide_input=[False,False,False,True]
+    # subdivide_input=[False,False,False,False,False]
+    # subdivide_input=[True,True,True,True,False]
+    time = [0.0,0.1,0.2,0.3,0.35,0.5,0.6,0.8,1.0]
+    wall_element_size_dx_store=[\
+        (2.0/64.0),\
+        (2.0/64.0),\
+        (2.0/64.0),\
+        (2.0/128.0),\
+        (1.0-9.9879544973373413e-01),\
+    ]
+    for i in range(0,n_paths):
+        filename_without_extension = "vorticity"
+        plot_title = labels_for_plot[i] #"$t=%1.2f$" % time[4]
+        plot_vorticity_plane(paths[i],filename_without_extension,file_extension,fig_directory,fig_prepre_fix[i],
+            subdivide=subdivide_input[i],
+            #title_label=plot_title,
+            fill_contour=False,
+            plot_reference_result=True,
+            shift_domain_by_minus_pi=False,
+            plot_at_wall=True,
+            plot_single_element_domain=False,
+            plot_wall_element_edge=False,wall_element_size_dx=wall_element_size_dx_store[i])
 
-file_extension="csv"
-n_paths=len(paths)
+if(True):
+    # simulation overview
+    paths=(
+    # filesystem+"NarvalFiles/2024_CSME/dipole_wall_collision/viscous_DWC_ILES_NSFR_cDG_IR_2PF_GL_OI-0_dofs0384_p5/solution_files/",\
+    filesystem+"NarvalFiles/2024_CSME/dipole_wall_collision/viscous_DWC_ILES_NSFR_cDG_IR_2PF_GL_OI-0_dofs0512_p7/solution_files/",\
+    )
+    files_per_path=[9,9]#[9,9]
 
-fig_directory = "./figures/2024_CSME"
-labels_for_plot=[\
-    # "$384^2$ P$5$ $c_{DG}$ NSFR.IR-GL",\
-    "$512^2$ P$7$ $c_{DG}$ NSFR.IR-GL",\
-]
-fig_prepre_fix = [\
-    # "384_p5_NSFR_cDG_IR_GL_",\
-    "512_p7_NSFR_cDG_IR_GL_",\
-]
-shift_domain_by_minus_pi_store=[False,False]
-time = [0.0,0.1,0.2,0.3,0.35,0.5,0.6,0.8,1.0]
+    file_extension="csv"
+    n_paths=len(paths)
 
-for i in range(0,n_paths):
-    for j in range(0,files_per_path[i]):
-            filename_without_extension = "vorticity_%i" % j
-            plot_title = "$t=%1.2f$" % time[j]
-            plot_vorticity_plane(paths[i],filename_without_extension,file_extension,fig_directory,fig_prepre_fix[i],
-                subdivide=False,title_label=plot_title,fill_contour=True,plot_reference_result=False,
-                shift_domain_by_minus_pi=shift_domain_by_minus_pi_store[i],
-                plot_single_element_domain=False,
-                color_bar_off=True)
+    fig_directory = "./figures/2024_CSME"
+    labels_for_plot=[\
+        # "$384^2$ P$5$ $c_{DG}$ NSFR.IR-GL",\
+        "$512^2$ P$7$ $c_{DG}$ NSFR.IR-GL",\
+    ]
+    fig_prepre_fix = [\
+        # "384_p5_NSFR_cDG_IR_GL_",\
+        "512_p7_NSFR_cDG_IR_GL_",\
+    ]
+    shift_domain_by_minus_pi_store=[False,False]
+    time = [0.0,0.1,0.2,0.3,0.35,0.5,0.6,0.8,1.0]
+
+    for i in range(0,n_paths):
+        for j in range(0,files_per_path[i]):
+                filename_without_extension = "vorticity_%i" % j
+                plot_title = "$t=%1.2f$" % time[j]
+                plot_vorticity_plane(paths[i],filename_without_extension,file_extension,fig_directory,fig_prepre_fix[i],
+                    subdivide=False,
+                    #title_label=plot_title,
+                    fill_contour=False,plot_reference_result=False,
+                    shift_domain_by_minus_pi=shift_domain_by_minus_pi_store[i],
+                    plot_single_element_domain=False,
+                    color_bar_off=True)
